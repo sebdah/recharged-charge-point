f4dc954f10cc58a458fe7030c771a57394c0ca95
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-05-18 20:21:42 +0200

    Fixed typo

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/url"
 
 	goLogging "github.com/op/go-logging"
@@ -9,19 +11,50 @@ import (
 	"github.com/sebdah/recharged-shared/websockets"
 )
 
-var log goLogging.Logger
+var (
+	log      goLogging.Logger
+	WsClient *websockets.Client
+)
 
 func main() {
 	// Configure logging
 	logging.Setup()
 
-	log.Info("Staring re:charged charge-point simulator")
+	log.Info("Starting re:charged charge-point simulator")
 	log.Info("Environment: %s", config.Env)
+
+	// Parse command line options
+	action := flag.String("action", "", "OCPP action")
+	flag.Parse()
 
 	// Connect to the WebSockets endpoint
 	log.Info("port: %d", config.Config.GetInt("port"))
 	log.Info(config.Config.GetString("central-system.endpoint-ocpp20j"))
 	wsEndpoint, _ := url.Parse(config.Config.GetString("central-system.endpoint-ocpp20j"))
 	log.Debug("Connecting to %s over websockets", wsEndpoint.String())
-	_ = websockets.NewClient(wsEndpoint)
+	WsClient = websockets.NewClient(wsEndpoint)
+
+	// Start the websockets communicator
+	go websocketsCommunicator()
+
+	// Send the actions
+	if *action == "authorize" {
+		WsClient.WriteMessage <- `[2, "1234", "Authorize", { "idTag": { "id": "11234" } }]`
+	}
+
+	// Do not terminate
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+}
+
+// Websockets message parser
+func websocketsCommunicator() {
+	var recv_msg string
+
+	for {
+		recv_msg = <-WsClient.ReadMessage
+
+		log.Info("Received message: %s\n", recv_msg)
+	}
 }
